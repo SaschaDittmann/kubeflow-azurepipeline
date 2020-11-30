abe6ce8adb0c8b160d8b87bf533b3c645f91820d
Sascha Dittmann <Sascha5812@gmx.de>
2020-11-30 10:36:20 +0100

    Update base image & remove depricated parameter

--- code/profile/profile.sh
+++ code/profile/profile.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-while getopts "m:n:i:d:s:p:u:r:w:t:b:" option;
+while getopts "m:n:i:s:p:u:r:w:t:b:" option;
     do
     case "$option" in
         m ) MODEL=${OPTARG};;
         n ) PROFILE_NAME=${OPTARG};;
         i ) INFERENCE_CONFIG=${OPTARG};;
-        d ) DATA=${OPTARG};;
         s ) SERVICE_PRINCIPAL_ID=${OPTARG};;
         p ) SERVICE_PRINCIPAL_PASSWORD=${OPTARG};;
         u ) SUBSCRIPTION_ID=${OPTARG};;
@@ -16,6 +15,6 @@ while getopts "m:n:i:d:s:p:u:r:w:t:b:" option;
     esac
 done
 az login --service-principal --username ${SERVICE_PRINCIPAL_ID} --password ${SERVICE_PRINCIPAL_PASSWORD} -t $TENANT_ID
-az ml model profile -n $PROFILE_NAME -m ${MODEL}:1 --ic $INFERENCE_CONFIG -d $DATA -t myprofileresult.json -w $WORKSPACE -g $RESOURCE_GROUP
+az ml model profile -n $PROFILE_NAME -m ${MODEL}:1 --ic $INFERENCE_CONFIG -t myprofileresult.json -w $WORKSPACE -g $RESOURCE_GROUP
 mv myprofileresult.json ${BASE_PATH}/myprofileresult.json
 echo ${BASE_PATH}
